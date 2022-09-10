d797c41527eb143e94099758432e865c1c2c11f7
g-rmc <g.rmc3000@gmail.com>
2022-09-10 00:14:19 -0300

    Rota POST /register configurada

--- src/controller/register.controller.js
+++ src/controller/register.controller.js
@@ -1,9 +1,17 @@
 import express from "express";
+import dayjs from "dayjs";
+import joi from "joi";
+import { stripHtml } from 'string-strip-html';
 import { db } from "../database/db.js";
 
+const newRegisterSchema = joi.object({
+    userId: joi.string().required(),
+    type: joi.string().valid('positive', 'negative').required(),
+    name: joi.string().trim().min(1).required(),
+    value: joi.number().required()
+})
+
 async function getUserRegisters(req,res) {
-
-
 
     console.log('Foi aqui')
     res.sendStatus(200);
@@ -11,6 +19,28 @@ async function getUserRegisters(req,res) {
 
 async function createRegister(req,res) {
 
+    const { userId, type, name, value } = req.body;
+    const date = dayjs().format('DD-MM').replace("-","/");
+
+    const validation = newRegisterSchema.validate({userId, type, name, value});
+    if (validation.error){
+        return res.status(422).send(validation.error.details.map(err => err.message));
+    }
+
+    const newRegister = {
+        userId,
+        type,
+        name: stripHtml(name).result,
+        value: Number(value).toFixed(2),
+        date
+    }
+
+    try {
+        await db.collection('register').insertOne(newRegister);
+        return res.sendStatus(201);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 }
 
 export { getUserRegisters, createRegister };
