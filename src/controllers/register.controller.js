0e8b2df0aa70ab2346e23456e17db424c93b1c37
g-rmc <g.rmc3000@gmail.com>
2022-09-12 12:12:14 -0300

    Inclusão da operação DELETE para a rota /register

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb';
 import { stripHtml } from 'string-strip-html';
 
 import { db } from "../db/db.js";
@@ -40,4 +41,27 @@ async function createRegister(req,res) {
     }
 }
 
-export { getUserRegisters, createRegister };
+async function deleteRegister(req,res) {
+
+    const userId = res.locals.userId.toString();
+    const registerId = stripHtml(req.params.ID_REGISTER).result;
+
+    try {
+        ObjectId(registerId)
+    } catch (error) {
+        return res.status(422).send('id inválido')
+    }
+
+    try {
+        const register = await db.collection('register').findOne({$and: [{userId: userId},{_id: ObjectId(registerId)}]});
+        if (!register){
+            return res.sendStatus(404);
+        }
+        await db.collection('register').deleteOne({$and: [{userId: userId},{_id: ObjectId(registerId)}]});
+        return res.sendStatus(200);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+}
+
+export { getUserRegisters, createRegister, deleteRegister };
--- src/routers/registers.router.js
+++ src/routers/registers.router.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUserRegisters, createRegister } from "../controllers/register.controller.js";
+import { getUserRegisters, createRegister, deleteRegister } from "../controllers/register.controller.js";
 import { userAuthorization } from "../middlewares/authorization.middleware.js";
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.use(userAuthorization);
 
 router.get('/register', getUserRegisters);
 router.post('/register', createRegister);
+router.delete('/register/:ID_REGISTER', deleteRegister);
 
 export default router;
